5145a62053e9e0ea563b9c0d622fe3f20634a4f1
王皓鋆 <wanghaoyun@gzsendi.cn>
2022-10-13 14:37:32 +0800

    769 [中等] -----审题不严谨

---
+++ leetcode/editor/cn/P769_MaxChunksToMakeSorted.java
@@ -0,0 +1,37 @@
+package leetcode.editor.cn;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 最多能完成排序的块
+ *
+ * @author why
+ * @date 2022-10-13 11:36:34
+ */
+public class P769_MaxChunksToMakeSorted {
+    public static void main(String[] args) {
+        Solution solution = new P769_MaxChunksToMakeSorted().new Solution();
+        solution.maxChunksToSorted(new int[]{1,0,2,3,4});
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int maxChunksToSorted(int[] arr) {
+            int ans = 0, n = arr.length;
+            for (int i = 0; i < n; i++) {
+                if (arr[i] > i) {
+                    int max = arr[i];
+                    while (i < max) max = Math.max(arr[++i], max);
+                    ans++;
+                }else if (arr[i]==i){
+                    ans++;
+                }
+            }
+            return ans;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
