0a3e345eb2e8f91a8d95671fd9ec909b3d769521
王皓鋆 <wanghaoyun@gzsendi.cn>
2022-10-18 15:00:24 +0800

    代码清理

--- leetcode/editor/cn/P904_FruitIntoBaskets.java
+++ leetcode/editor/cn/P904_FruitIntoBaskets.java
@@ -46,35 +46,4 @@ public class P904_FruitIntoBaskets {
         }
     }
 //leetcode submit region end(Prohibit modification and deletion)
-
-}
-class Solution {
-    public int totalFruit(int[] fruits) {
-        int ans = 0, tempAns = 0, n = fruits.length;
-        int[] box = new int[2];
-        box[0] = -1;
-        box[1] = -1;
-
-        int l = 0, r = 0;
-        while (r < n) {
-            if (fruits[r] != box[0] && fruits[r] != box[1] && box[0] != -1 && box[1] != -1) {
-                if (fruits[l] == box[0]) {
-                    box[1] = fruits[r];
-                } else {
-                    box[0] = fruits[r];
-                }
-                ans = Math.max(tempAns, ans);
-                tempAns = r - l;
-                continue;
-            }
-
-            if (box[0] == -1 && box[1] != fruits[r]) box[0] = fruits[r];
-            if (box[1] == -1 && box[0] != fruits[r]) box[1] = fruits[r];
-            if (box[0] != -1 && box[1] != -1 && fruits[l] != fruits[r]) l = r;
-            r++;
-            tempAns++;
-        }
-        ans = Math.max(ans, tempAns);
-        return ans;
-    }
 }
