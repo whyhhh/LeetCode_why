07d94f47faab5004203399465d098ca222580ccc
王皓鋆 <wanghaoyun@gzsendi.cn>
2022-11-11 23:29:24 +0800

    1668 [简单] (dp)

---
+++ leetcode/editor/cn/P1668_MaximumRepeatingSubstring.java
@@ -0,0 +1,30 @@
+package leetcode.editor.cn;
+
+/**
+ * 最大重复子字符串
+ *
+ * @author why
+ * @date 2022-11-03 22:50:00
+ */
+public class P1668_MaximumRepeatingSubstring {
+    public static void main(String[] args) {
+        Solution solution = new P1668_MaximumRepeatingSubstring().new Solution();
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int maxRepeating(String sequence, String word) {
+            int ans = 0;
+            int n = sequence.length(), m = word.length();
+            int[] t = new int[n + 10];
+            for (int i = 1; i <= n; i++) {
+                if (i - m < n) continue;
+                if (sequence.substring(i - m, i).equals(word)) t[i] = t[i - m] + 1;
+                ans = Math.max(ans, t[i]);
+            }
+            return ans;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
