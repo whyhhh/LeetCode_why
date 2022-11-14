1d8fe932fe56573e1c4b23fda99800396a00e323
王皓鋆 <wanghaoyun@gzsendi.cn>
2022-11-14 17:44:23 +0800

    198 [中等] (状态机线性 DP)

--- leetcode/editor/cn/P198_HouseRobber.java
+++ leetcode/editor/cn/P198_HouseRobber.java
@@ -17,14 +17,16 @@ public class P198_HouseRobber {
             int n = nums.length;
             if (n == 1) return nums[0];
             if (n == 2) return Math.max(nums[0], nums[1]);
-            int[] dp = new int[n];
-            dp[0] = nums[0];
-            dp[1] = nums[1];
-
-            for (int i = 2; i < n; i++) {
-                dp[i] = Math.max(nums[i] + dp[i - 2], dp[i - 1] - nums[i - 1] + nums[i]);
+            int[][] dp = new int[n][2];
+            //0：偷当前，1：不偷当前
+            dp[0][0] = nums[0];
+            dp[0][1] = 0;
+            for (int i = 1; i < n; i++) {
+                dp[i][0] = dp[i - 1][1] + nums[i];
+                dp[i][1] = Math.max(dp[i - 1][0], dp[i - 1][1]);
             }
-            return Math.max(dp[n - 1], dp[n - 2]);
+
+            return Math.max(dp[n - 1][0], dp[n - 1][1]);
         }
     }
 //leetcode submit region end(Prohibit modification and deletion)
