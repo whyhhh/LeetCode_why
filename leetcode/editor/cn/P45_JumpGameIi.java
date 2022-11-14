24dce6e7ee4e2190f8e9fdc86a08950e6219877a
王皓鋆 <wanghaoyun@gzsendi.cn>
2022-11-14 16:45:51 +0800

    45 [中等] 多做几遍

--- leetcode/editor/cn/P45_JumpGameIi.java
+++ leetcode/editor/cn/P45_JumpGameIi.java
@@ -9,19 +9,19 @@ package leetcode.editor.cn;
 public class P45_JumpGameIi {
     public static void main(String[] args) {
         Solution solution = new P45_JumpGameIi().new Solution();
-        solution.jump(new int[]{2, 3, 1, 1, 4});
+        solution.jump(new int[]{2, 2, 3, 1, 4,1});
     }
 
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
         public int jump(int[] nums) {
             int n = nums.length;
-            int[] f = new int[n];
+            int[] dp = new int[n];
             for (int i = 1, j = 0; i < n; i++) {
                 while (j + nums[j] < i) j++;
-                f[i] = f[j] + 1;
+                dp[i] = dp[j] + 1;
             }
-            return f[n - 1];
+            return dp[n - 1];
         }
     }
 //leetcode submit region end(Prohibit modification and deletion)
