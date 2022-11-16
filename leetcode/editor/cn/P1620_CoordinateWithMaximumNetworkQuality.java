22c0fb64056a09eedbcd54b0858993b4d5020492
王皓鋆 <wanghaoyun@gzsendi.cn>
2022-11-16 15:15:31 +0800

    1620 [中等] (简单模拟)

--- leetcode/editor/cn/P1620_CoordinateWithMaximumNetworkQuality.java
+++ leetcode/editor/cn/P1620_CoordinateWithMaximumNetworkQuality.java
@@ -15,29 +15,26 @@ public class P1620_CoordinateWithMaximumNetworkQuality {
     class Solution {
         public int[] bestCoordinate(int[][] towers, int radius) {
             int x = 0, y = 0, max = 0;
-            int[][] t = new int[110][110];
+            int[][] temp = new int[110][110];
             for (int[] tower : towers) {
                 int a = tower[0], b = tower[1], q = tower[2];
-                for (int i = Math.max(0, a - radius); i <= a + radius; i++) {
-                    for (int j = Math.max(0, b - radius); j <= b + radius; j++) {
-                        double r = Math.sqrt((a - i) * (a - i) + (b - j) * (b - j));
-                        if (r > radius) continue;
-                        t[i][j] += Math.floor(q / (1 + r));
-                        if (t[i][j] > max) {
+                for (int i = Math.max(a - radius, 0); i <= a + radius; i++) {
+                    for (int j = Math.max(b - radius, 0); j <= b + radius; j++) {
+                        double sqrt = Math.sqrt((a - i) * (a - i) + (b - j) * (b - j));
+                        if (sqrt > radius) continue;
+                        temp[i][j] += q / (1 + sqrt);
+                        if (max < temp[i][j]) {
                             x = i;
                             y = j;
-                            max = t[i][j];
-                        } else if (t[i][j] == max && (i < x || (i == x && j < y))) {
+                            max = temp[i][j];
+                        } else if (max == temp[i][j] && ((i < x) || (i == x && j < y))) {
                             x = i;
                             y = j;
                         }
-
                     }
                 }
-
             }
             return new int[]{x, y};
-
         }
     }
 //leetcode submit region end(Prohibit modification and deletion)
