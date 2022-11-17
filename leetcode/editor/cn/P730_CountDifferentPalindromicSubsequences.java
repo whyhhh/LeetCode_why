3f034ffe410aa9377cb3e007207af074cd35beff
王皓鋆 <wanghaoyun@gzsendi.cn>
2022-11-17 16:02:48 +0800

    730 [困难] (区间dp) ----- 脑壳疼，明天再看两遍

---
+++ leetcode/editor/cn/P730_CountDifferentPalindromicSubsequences.java
@@ -0,0 +1,48 @@
+package leetcode.editor.cn;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+
+/**
+ * 统计不同回文子序列
+ *
+ * @author why
+ * @date 2022-11-17 11:20:31
+ */
+public class P730_CountDifferentPalindromicSubsequences {
+    public static void main(String[] args) {
+        Solution solution = new P730_CountDifferentPalindromicSubsequences().new Solution();
+        solution.countPalindromicSubsequences("abaaba");
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        int MOD = (int)1e9+7;
+        public int countPalindromicSubsequences(String s) {
+            char[] cs = s.toCharArray();
+            int n = cs.length;
+            int[][] f = new int[n][n];
+            int[] L = new int[4], R = new int[4];
+            Arrays.fill(L, -1);
+            for (int i = n - 1; i >= 0; i--) {
+                L[cs[i] - 'a'] = i;
+                Arrays.fill(R, -1);
+                for (int j = i; j < n; j++) {
+                    R[cs[j] - 'a'] = j;
+                    for (int k = 0; k < 4; k++) {
+                        if (L[k] == -1 || R[k] == -1) continue;
+                        int l = L[k], r = R[k];
+                        if (l == r) f[i][j] = (f[i][j] + 1) % MOD;
+                        else if (l == r - 1) f[i][j] = (f[i][j] + 2) % MOD;
+                        else f[i][j] = (f[i][j] + f[l + 1][r - 1] + 2) % MOD;
+                    }
+                }
+            }
+            return f[0][n - 1];
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
