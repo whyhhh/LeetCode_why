56286bc95aac8857fb148c19601fd09a0adfd448
王皓鋆 <wanghaoyun@gzsendi.cn>
2022-11-22 18:03:36 +0800

    334  [中等] (LIS问题) 多刷几遍

--- leetcode/editor/cn/P334_IncreasingTripletSubsequence.java
+++ leetcode/editor/cn/P334_IncreasingTripletSubsequence.java
@@ -11,24 +11,20 @@ import java.util.Arrays;
 public class P334_IncreasingTripletSubsequence {
     public static void main(String[] args) {
         Solution solution = new P334_IncreasingTripletSubsequence().new Solution();
-        solution.increasingTriplet(new int[]{20,100,10,12,5,13});
+        solution.increasingTriplet(new int[]{6, 7, 1, 2});
     }
 
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
         public boolean increasingTriplet(int[] nums) {
-            int[] max = new int[3];
-            Arrays.fill(max, Integer.MAX_VALUE);
-            for (int i = 0; i < nums.length; i++) {
-                for (int j = 0; j < max.length; j++) {
-                    if (nums[i] <= max[j]) {
-                        if (j == 2) {
-                            return true;
-                        }
-                        max[j] = nums[i];
-                        break;
-                    }
-                }
+            int n = nums.length;
+            long[] f = new long[2];
+            f[0] = f[1] = (long) 1e19;
+            for (int i = 0; i < n; i++) {
+                int cur = nums[i];
+                if (cur > f[1]) return true;
+                else if (f[0] < cur && cur < f[1]) f[1] = cur;
+                else if (f[0] > cur) f[0] = cur;
             }
             return false;
         }
