0aea0d68059ef9909a80d5af1ef24517038d472e
王皓鋆 <wanghaoyun@gzsendi.cn>
2022-11-23 14:08:56 +0800

    213  [中等] (dp) 复习

--- leetcode/editor/cn/P213_HouseRobberIi.java
+++ leetcode/editor/cn/P213_HouseRobberIi.java
@@ -18,30 +18,26 @@ public class P213_HouseRobberIi {
         public int rob(int[] nums) {
             int n = nums.length;
             if (n == 1) return nums[0];
-            int[][] dp = new int[n][2];
-            int res1, res2;
+            if (n == 2) return Math.max(nums[0], nums[1]);
+            int ans1, ans2;
 
-            //第一间选了
-            //0：偷，1：没偷
-            dp[0][0] = nums[0];
-            dp[0][1] = 0;
+            int[][] f = new int[n][2];
+            f[0][0] = nums[0];
             for (int i = 1; i < n - 1; i++) {
-                dp[i][0] = dp[i - 1][1] + nums[i];
-                dp[i][1] = Math.max(dp[i - 1][0], dp[i - 1][1]);
+                f[i][0] = f[i - 1][1] + nums[i];
+                f[i][1] = Math.max(f[i - 1][0], f[i - 1][1]);
             }
-            res1 = Math.max(dp[n - 2][0], dp[n - 2][1]);
+            ans1 = Math.max(f[n - 2][0], f[n - 2][1]);
 
-            //第一间没选
-            //0：偷，1：没偷
-            dp[0][0] = 0;
-            for (int i = 1; i < n; i++) {
-                dp[i][0] = dp[i - 1][1] + nums[i];
-                dp[i][1] = Math.max(dp[i - 1][0], dp[i - 1][1]);
+            f = new int[n][2];
+            f[1][0] = nums[1];
+            for (int i = 2; i < n; i++) {
+                f[i][0] = f[i - 1][1] + nums[i];
+                f[i][1] = Math.max(f[i - 1][0], f[i - 1][1]);
             }
-            res2 = Math.max(dp[n - 1][0], dp[n - 1][1]);
+            ans2 = Math.max(f[n - 1][0], f[n - 1][1]);
 
-
-            return Math.max(res1, res2);
+            return Math.max(ans1, ans2);
         }
     }
 //leetcode submit region end(Prohibit modification and deletion)
